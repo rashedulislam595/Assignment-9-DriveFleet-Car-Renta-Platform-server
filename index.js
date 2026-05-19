99f639c94846d212764bdae9d2b68a6dde00a028
rashedulislam595 <rashedulislam956581@gmail.com>
2026-05-19 11:23:22 +0600

    server connect mongodb and  create database name DriveFleet

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 dotenv.config()
 
 const PORT = process.env.PORT;
+const uri = process.env.MONGO_URI;
 
-app.get('/',(req,res)=>{
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const db = client.db("DriveFleet");
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('Server is running fine!')
 })
 
-app.listen(PORT,(req,res)=>{
+app.listen(PORT, (req, res) => {
     console.log(`Server running on port ${PORT}`)
 })
